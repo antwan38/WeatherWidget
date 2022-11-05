9e9eeece017fd2ad1f757dade5738624cf29a626
Antwan <antwansittard@gmail.com>
2022-11-05 22:16:21 +0100

    pff

--- src/test/java/com/Antwan/WeatherWidget/widgetServiceTest.java
+++ src/test/java/com/Antwan/WeatherWidget/widgetServiceTest.java
@@ -1,6 +1,7 @@
 package com.Antwan.WeatherWidget;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -8,11 +9,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.Assert;
 
+@RunWith(SpringRunner.class)
 @DataJpaTest
-//@TestPropertySource(locations = "classpath:test.properties")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@TestPropertySource(locations = "classpath:test.properties")
 public class widgetServiceTest {
 
 
