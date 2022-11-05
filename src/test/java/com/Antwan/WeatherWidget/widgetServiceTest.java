1f946378f5db1b29167dc85e9b551b3f225a87f9
Antwan <antwansittard@gmail.com>
2022-11-05 21:06:53 +0100

    test link

--- src/test/java/com/Antwan/WeatherWidget/widgetServiceTest.java
+++ src/test/java/com/Antwan/WeatherWidget/widgetServiceTest.java
@@ -1,15 +1,21 @@
 package com.Antwan.WeatherWidget;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.util.Assert;
 
-@SpringBootTest
+@DataJpaTest
+@TestPropertySource(locations = "classpath:test.properties")
 public class widgetServiceTest {
 
+    @Autowired
+    private TestEntityManager em;
     @Test
     void GetWidget(){
-
-        Assert.isTrue(true);
     }
 }
