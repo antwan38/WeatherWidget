6d0ce05ba04a7cd8a47309f029431340354410e7
Antwan <antwansittard@gmail.com>
2022-11-21 14:44:48 +0100

    style edit

--- src/main/java/com/Antwan/WeatherWidget/WeatherWidgetApplication.java
+++ src/main/java/com/Antwan/WeatherWidget/WeatherWidgetApplication.java
@@ -1,3 +1,6 @@
+/**
+ * Info about this package doing something for package-info.java file.
+ */
 package com.Antwan.WeatherWidget;
 
 import org.springframework.boot.SpringApplication;
@@ -5,9 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeatherWidgetApplication {
+	private WeatherWidgetApplication(){
 
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherWidgetApplication.class, args);
 	}
-
 }
