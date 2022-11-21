bd5051f4b04bd5b53823436c73232ebea83bb80f
Antwan <antwansittard@gmail.com>
2022-11-21 15:02:27 +0100

    add packige info

--- src/main/java/com/Antwan/WeatherWidget/WeatherWidgetApplication.java
+++ src/main/java/com/Antwan/WeatherWidget/WeatherWidgetApplication.java
@@ -8,9 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeatherWidgetApplication {
-	private WeatherWidgetApplication(){
 
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherWidgetApplication.class, args);
 	}
--- src/main/java/com/Antwan/WeatherWidget/package-info.java
+++ src/main/java/com/Antwan/WeatherWidget/package-info.java
@@ -1,1 +1,8 @@
+/**
+ * This module is providing weather data
+ *
+ * @since 1.0
+ * @author Antwan
+ * @version 1.1
+ */
 package com.Antwan.WeatherWidget;
