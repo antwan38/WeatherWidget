4db0057497b91846f61ee2e794b8393c428cd778
Antwan <antwansittard@gmail.com>
2022-11-21 15:20:57 +0100

    added reports folder for superlinter

--- src/main/java/com/Antwan/WeatherWidget/controller/GridController.java
+++ src/main/java/com/Antwan/WeatherWidget/controller/GridController.java
@@ -2,9 +2,14 @@ package com.Antwan.WeatherWidget.controller;
 
 import com.Antwan.WeatherWidget.model.WidgetData;
 import com.Antwan.WeatherWidget.service.widgetService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
-import java.lang.constant.Constable;
+
 import java.util.List;
 import java.util.Map;
 
---
+++ src/main/java/com/Antwan/WeatherWidget/repository/package-info.java
@@ -0,0 +1,1 @@
+package com.Antwan.WeatherWidget.repository;
