5817bc5cc170458606fc55f7c778b8651a95a718
Antwan <antwansittard@gmail.com>
2022-11-22 21:04:34 +0100

    big bruh

--- src/test/java/com/Antwan/WeatherWidget/WidgetServiceTest.java
+++ src/test/java/com/Antwan/WeatherWidget/WidgetServiceTest.java
@@ -1,9 +1,7 @@
 package com.Antwan.WeatherWidget;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.TestPropertySource;
 
 
